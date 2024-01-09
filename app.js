0fe3841a57ea53d1d0af7fe5b9da3f9be3b65894
aleksaks27 <aleksaks27@gmail.com>
2024-01-09 19:36:38 +0000

    Add error catching and vastly improve app styling

--- app.js
+++ app.js
@@ -1,49 +1,71 @@
 const button = document.querySelector("button");
 const input = document.querySelector("input");
-const place = document.querySelector("#place");
+const container = document.querySelector("#container");
+const place = document.querySelector("#location");
+const icon = document.querySelector("#icon");
+const headline = document.querySelector("#headline");
 const time = document.querySelector("#time");
 const temperature = document.querySelector("#temperature");
+const feelsLike = document.querySelector("#feelsLike");
 const rain = document.querySelector("#rain");
-const wind = document.querySelector("#wind");
+const humidity = document.querySelector("#humidity");
+const windSpeed = document.querySelector("#wind");
+const windDirection = document.querySelector("#windDirection");
 
+let locationValid = true;
 let information = {};
 
 const getAllData = async function(city) {
     const weather = await fetch('https://api.weatherapi.com/v1/current.json?key=e1dc5b3aa4464ace8f7224321240401&q=' + city);
     const response = await weather.json();
-
-    return response
+    return response;
 }
 
 const getSpecificData = async function(city){
-    let data = await getAllData(city);
-    information = {
-        city: data["location"]["name"],
-        country: data["location"]["country"],
-        time: data["location"]["localtime"],
-        temperature: data["current"]["temp_c"],
-        temperature_feelslike: data["current"]["feelslike_c"],
-        rain: data["current"]["precip_mm"],
-        humidity: data["current"]["humidity"],
-        wind_speed: data["current"]["wind_mph"],
-        wind_direction: data["current"]["wind_dir"]
-    };
+    try{
+        locationValid = true;
+        let data = await getAllData(city);
+        information = {
+            city: data["location"]["name"],
+            country: data["location"]["country"],
+            time: data["location"]["localtime"],
+            icon: data["current"]["condition"]["icon"],
+            headline: data["current"]["condition"]["text"],
+            temperature: data["current"]["temp_c"],
+            feelsLike: data["current"]["feelslike_c"],
+            rain: data["current"]["precip_mm"],
+            humidity: data["current"]["humidity"],
+            windSpeed: data["current"]["wind_mph"],
+            windDirection: data["current"]["wind_dir"]
+        };
+    } catch{
+        locationValid = false;
+        alert("Please enter a valid location!");
+    }
 
-    console.log(information);
 }
 
 const updateDisplay = function() {
-    place.textContent = `City: ${information.city}, Country: ${information.country}`;
-    time.textContent = `Local Time: ${information.time}`;
-    temperature.textContent = `Temperature: ${information.temperature}, Feels like: ${information.temperature_feelslike}`;
-    rain.textContent = `Rain: ${information.rain}mm, Humidity: ${information.humidity}%`
-    wind.textContent = `Wind speed: ${information.wind_speed}mph, Wind direction: ${information.wind_direction}`;
+    place.textContent = `${information.city}, ${information.country}`;
+    time.textContent = `${information.time}`;
+    headline.textContent = `${information.headline}`;
+    icon.innerHTML = `<img src="https:${information.icon}">`;
+    temperature.innerHTML = `<p><b>Temperature</b>: ${information.temperature}°C</p>`;
+    feelsLike.innerHTML = `<p><b>Feels like</b>: ${information.feelsLike}°C</p>`;
+    rain.innerHTML = `<p><b>Rain</b>: ${information.rain}mm/hour</p>`;
+    humidity.innerHTML = `<p><b>Humidity</b>: ${information.humidity}%</p>`;
+    wind.innerHTML = `<p><b>Wind speed</b>: ${information.windSpeed}mph</p>`;
+    windDirection.innerHTML = `<p><b>Wind direction</b>: ${information.windDirection}</p>`;
 }
 
-getSpecificData("london");
-updateDisplay();
+button.addEventListener("click", async () => {
+        await getSpecificData(input.value);
+        if (locationValid) {
+            updateDisplay();
+        }
+});
 
-button.addEventListener("click", () => {
-    getSpecificData(input.value);
+document.addEventListener('DOMContentLoaded', async () => {
+    await getSpecificData("London");
     updateDisplay();
 });
